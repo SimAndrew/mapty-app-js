4d6a11189ae34a248633ee6f8132c9cedd4e03df
AndSim <siminiucandrew@gmail.com>
2023-08-22 13:55:55 +0300

    feat: display map and then center on current position of user. (library google leaflet)

--- script.js
+++ script.js
@@ -14,11 +14,17 @@ if (navigator.geolocation) {
   navigator.geolocation.getCurrentPosition(function(position) {
     const { latitude } = position.coords;
     const { longitude } = position.coords;
+    const coords = [latitude, longitude];
 
-    console.log(longitude, latitude);
+    const map = L.map('map').setView(coords, 13);
 
-    console.log(`https://www.google.com/maps/search/?api=1&query=${latitude},${longitude}`);
+    L.tileLayer('https://tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
 
+    L.marker(coords).addTo(map)
+      .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+      .openPopup();
   }, function() {
     alert('Could not get current position');
   })
